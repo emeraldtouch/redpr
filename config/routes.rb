1be4660d5c882c2f89dcfa5f9a8702c1af7ce9e7
Andrew N.Romanov <andrew.n.romanov@gmail.com>
2019-12-20 02:48:10 +0300

    Separate gems.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
+class ActionDispatch::Routing::Mapper
+  def draw(routes_name)
+    instance_eval(File.read(Rails.root.join("config/routes/#{routes_name}.rb")))
+  end
+end
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  draw :api
+  draw :app
+  draw :users
+
+  namespace :admin do
+    draw :admin
+  end
+
+
 end
